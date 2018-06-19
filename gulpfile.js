5343e13e37340b991f4e6d2a444091fb781849df
Stefan <k1155240@students.jku.at>
2018-06-19 20:14:20 +0200

    Cloud ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var connect = require('gulp-connect');
+
+gulp.task('serve', function() {
+    connect.server({
+      root: "./",
+      port: process.env.PORT || 30000
+    });
+  });
 
 gulp.task('browser-sync', function() {
     browserSync({
@@ -9,4 +17,4 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('default', ['browser-sync']);
+gulp.task('default', ['serve']);
